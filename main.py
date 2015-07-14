8b93e9a59a50790e89327cadb12e693a863315b0
James Shawn Carnley <Shawn.Carnley@gatech.edu>
2015-07-14 12:52:29 -0400

    Fixed frame preview window.

--- main.py
+++ main.py
@@ -1,68 +1,49 @@
 #!/usr/bin/python
- 
-# math stuff
 import numpy as np
 import scipy.ndimage as nd
 from random import randint
 
-# data stuff
+
 from cStringIO import StringIO
 import PIL.Image
 from google.protobuf import text_format
-# system stuff
+
 import os
 import sys
 import argparse
-# a little sauce
+
 import caffe
 from deepdream import deepdream, net
 import random
-#import blobs
-
-
+import cv2
 
 ###############################################################################
-# openCV stuff
+# openCV Preview Window
 # ------------
-# this is a decent way to show an image in a window (or over a network via X),
-# but it's a huge pain in the ass to convert between cv2 and PIL and i have
-# not been successful thus far.
-#
-# from what i've read, PIL is dead and openCV is standard now, so
-# we should probably work towards phasing out PIL and using openCV instead. it
-# has support for all languages and is probably faster than PIL.
-#
-# plus it has a ton of cool features like face detection, motion, image
-# resizing, transformations, edge detection, various neat effects, webcams...
 ##############################################################################
-import cv2
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.namedWindow('image_process', cv2.WINDOW_AUTOSIZE)
 def show(img):
-  open_cv_image = img[:, :, ::-1].copy() #.view(dtype=np.uint8)
-  print(open_cv_image)
-  cv2.imshow('image', open_cv_image.view(dtype=np.uint8))
-  cv2.waitKey(30)
-
+	open_cv_image = cv2.cvtColor(np.array(img),cv2.COLOR_RGB2BGR) 
+	open_cv_image = open_cv_image[:, :, ::-1].copy() 
+	cv2.imshow('image_process', open_cv_image.view())
+	cv2.waitKey(25)
 
 ##############################################################################
-# main function
+# Main function
 # -------------
-# don't ask, __name__ == '__main__' is just how python do.
-#
-# passing in arguments is totally 1337 and allows other applications to
-# interface with us. this means that we can automate stuff! yeah!
-# try not to commit any hardcoded values. if possible, you should be
-# passing in all parameters via command line and then setting defaults if they
-# do not exist. argparse is a great library for this, i just did it the quick
-# and dirty way because it was late. 
-# 
-# TODO: parametrize stuff
-#          --scaleCoef
-#         --iterations
-#         --whatever parameters deepdream.deepdream has
-#       break out as many tedious functions as possible and put them in a
-#         utils.py or something
+# Usage: Usage: $ python main.py -f [source/filename.jpg] -o [output dir] 
+#                                -s [scale] -i [iterations] -b [all/blobname] 
+#                                -z [0/1]
+# Arguments:
+# '-f', '--filename'  : Input file
+# ''-o', '--outputdir': Output directory
+# '-s', '--scaleCoef' : Scale Coefficient (default=0.5)
+# '-i', '--iterations': Iterations (default=100)
+# '-b', '--blob'      : Blob name (default=random)
+# '-z', '--zoom'      : Zoom (default=0)
+# '-p', '--preview'   : Preview Window (default=0)
 ##############################################################################
+
 if __name__ == '__main__':
   # get  args if we can.
   parser = argparse.ArgumentParser()
@@ -72,8 +53,9 @@ if __name__ == '__main__':
   parser.add_argument('-i', '--iterations', default=100, type=int)
   parser.add_argument('-b', '--blob', default=random.choice(net.blobs.keys()), type=str)
   parser.add_argument('-z', '--zoom', default=0, type=int)
+  parser.add_argument('-p', '--preview', default=0, type=int)
   args = parser.parse_args()
-
+  
   if args.filename == None:
     print 'Error: No source file'
     print 'Usage: $ python main.py -f [source/filename.jpg] -o [output dir] -s [scale] -i [iterations] -b [all/blobname] -z [0/1]'
@@ -82,7 +64,6 @@ if __name__ == '__main__':
   # PIL is stupid, go away PIL
   img = np.float32(PIL.Image.open(args.filename))
   print 'Loaded', args.filename
-
 
   # split file name so we can make a special folder
   fn = args.filename.split('/')[-1].split('.')
@@ -93,26 +74,10 @@ if __name__ == '__main__':
   # make sure output path exists
   if not os.path.exists(args.outputdir):
     os.makedirs(args.outputdir)
-
-  # # for the love of god, lets not crash after the first
-  # # deepdream due to an ENOENT error... (-_-)
-  # show(img)
+  
   print "Output: ", framepath
   if not os.path.exists(framepath):
       os.makedirs(framepath)
-
-
-  # see ya on the other side
-  frame = img
-  h, w = frame.shape[:2]
-  s = args.scaleCoef # scale coefficient
-  
-  # # code interaction is awesome!
-  # # uncomment this to fondle the data on open
-  # # useful so you don't run for hours with faulty params
-  # # (ctrl+d continues execution)
-  # import code
-  # code.interact(local=locals())
 
   # see ya on the other side
   frame = img
@@ -124,6 +89,11 @@ if __name__ == '__main__':
       PIL.Image.fromarray(np.uint8(frame)).save(framepath+'/source.'+ext)
       j = 0
       for blob in net.blobs.keys():
+          
+          #Show preview window
+          if args.preview == 1:
+  			show(PIL.Image.fromarray(np.uint8(frame)))
+          
           safeblob = blob.replace('/', '-')
           try:
             # if we've already generated this image, then don't bother
@@ -138,10 +108,13 @@ if __name__ == '__main__':
             pass
           except KeyError as err:
             print 'KeyError:', str(blob), err
-
   else:
       safeblob = args.blob.replace('/', '-')
       for i in xrange(args.iterations):
+          #Show preview window
+          if args.preview == 1:
+  			show(PIL.Image.fromarray(np.uint8(frame)))
+          
           # save the original as 000.ext and hallucinations as 00i.ext
           # this also checks the save path so that we don't crash after 1 deepdream
           PIL.Image.fromarray(np.uint8(frame)).save(framepath+'/'+safeblob+'--'+str(i).zfill(3)+'.'+ext)
